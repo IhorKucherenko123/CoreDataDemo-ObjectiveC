00c9e277817641640d18374d87bebacd12f561c0
Ihorkucherenko123 <ihorkucherenko123@gmail.com>
2018-06-12 10:30:30 +0800

    core_data_demo

---
+++ CoreDataObjectiveC/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  CoreDataObjectiveC
+//
+//  Created by VKS on 5/1/16.
+//  Copyright © 2016 VKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Person.h"
+
+@interface MainViewController : UITableViewController
+
+-(void)reloadData;
+
+@end
